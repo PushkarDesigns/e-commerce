0a615d989cde5d1cd3f292ce34dd388d128d767b
Pushkar <rawatprince815@gmail.com>
2025-12-11 13:59:50 +0530

    hero section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,14 +13,14 @@ const App = () => {
   const { setIsSeller, showUserLogin } = useContext(AppContext);
   const isSellerPath = useLocation().pathname.includes('seller');
   return (
-    <div>
+    <div className="text-default min-h-screen">
       {
         isSellerPath ? null : <Navbar />
       }
       {
         showUserLogin ? <Auth /> : null
       }
-      <div className="">
+      <div className="px-6 md:px-16 lg:px-24 xl:px-32">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='relative'>
+      <img src={assets.main_banner_bg} alt="" className='hidden md:block w-full' />
+    </div>
   )
 }
 
