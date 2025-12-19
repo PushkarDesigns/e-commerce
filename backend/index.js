f925e2c9d3b087bab1063e90f4dc1a5aa72ea67a
Pushkar <rawatprince815@gmail.com>
2025-12-19 11:31:41 +0530

    backend...

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const allowedOrigins = ["http://localhost:5173"];
+
+// middlewares
+app.use(express.json());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cookieParser());
+
+// Api Endpointes
+app.get('/',(req, res) => {
+    res.send("hello");
+});
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
