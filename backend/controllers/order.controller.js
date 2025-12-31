fb8a2d3ad64e0f908adf5e40bf507bd021475ab8
Pushkar <rawatprince815@gmail.com>
2025-12-31 11:14:32 +0530

    stripe payment...

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,11 +1,47 @@
 import Order from "../models/order.model.js";
 import Product from "../models/product.model.js";
-// Place order COD: /api/order/cod
 
+
+// Place order COD: /api/order/cod
 export const placeOrderCOD = async (req, res) => {
   try {
     const userId = req.user;
     const { items, address } = req.body;
+    if (!items || !address) {
+      return res
+        .status(400)
+        .json({ message: "Items and address are required", success: false });
+    }
+    let amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+      return (await acc) + product.offerPrice * item.quantity;
+    }, 0);
+    amount += Math.floor((amount * 2) / 100);
+    // create Order
+    await Order.create({
+      userId,
+      items,
+      address,
+      amount,
+      paymentType: "COD",
+      isPaid: false,
+    });
+    res.status(201).json({
+      message: "Order placed successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.error("Error placing order:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Place order stripe: /api/order/stripe
+export const placeOrderStripe = async (req, res) => {
+  try {
+    const userId = req.user;
+    const { items, address } = req.body;
+    
     if (!items || !address) {
       return res
         .status(400)
